4a7acc39d49f8e0961a5b464e1326f821e45dd90
Mubashir <mubashir.ansari@digitalsalt.in>
2024-05-29 11:08:38 +0530

    new changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
       repo: 'git@github.com:digitalsalt-tech/asiaimpact-api.git',
       path: '/home/ubuntu',
       'pre-deploy-local': '',
-      'post-deploy': 'source ~/.nvm/nvm.sh && npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
       'ssh_options': 'ForwardAgent=yes'
     }
