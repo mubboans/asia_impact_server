ac95e586e2e71110a3db483902bb32852989d560
Mubashir <mubashir.ansari@digitalsalt.in>
2024-05-30 13:24:46 +0530

    changes in eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,9 +16,10 @@ module.exports = {
       host: '54.161.183.125',
       ref: 'origin/main',
       repo: 'git@github.com:digitalsalt-tech/asiaimpact-api.git',
-      path: '/home/ubuntu/asiaimpact-api', // Ensure this path exists
+      path: '/home/ubuntu/asiaimpact-api',
       'pre-deploy-local': '',
-      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-deploy': 'git fetch --all && git reset --hard origin/main',
+      'post-deploy': 'cd /home/ubuntu/asiaimpact-api && cp -a source/. . && echo "Copy successful" || echo "Copy failed" && npm install && echo "NPM install successful" || echo "NPM install failed" && pm reload ecosystem.config.js --env production && echo "PM2 reload successful" || echo "PM2 reload failed" && rm -rf source && echo "Source removal successful" || echo "Source removal failed"',
       'pre-setup': '',
       'ssh_options': 'ForwardAgent=yes'
     }
