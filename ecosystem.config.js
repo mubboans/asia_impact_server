0764e0accdf7dae506ade16e282148a422fc2f01
Mubashir <mubashir.ansari@digitalsalt.in>
2024-05-30 13:20:32 +0530

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       repo: 'git@github.com:digitalsalt-tech/asiaimpact-api.git',
       path: '/home/ubuntu/asiaimpact-api',
       'pre-deploy-local': '',
-      'pre-deploy': 'git fetch --all && git reset --hard origin/main',
+      // 'pre-deploy': 'git fetch --all && git reset --hard origin/main',
       'post-deploy': 'cd /home/ubuntu/asiaimpact-api && cp -a source/. . && echo "Copy successful" || echo "Copy failed" && npm install && echo "NPM install successful" || echo "NPM install failed" && pm reload ecosystem.config.js --env production && echo "PM2 reload successful" || echo "PM2 reload failed" && rm -rf source && echo "Source removal successful" || echo "Source removal failed"',
       'pre-setup': '',
       'ssh_options': 'ForwardAgent=yes'
